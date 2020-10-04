6a1680b13efc4a774cc79f67209bd4197e42a95b
nhy0052 <71787808+nhy0052@users.noreply.github.com>
2020-10-04 21:24:41 +0900

    1004 수업 미션 혜연

--- hyeyeon/1004_class_mission.cpp
+++ hyeyeon/1004_class_mission.cpp
@@ -3,19 +3,24 @@
 
 int main()
 {
-	char c1;
-	printf("문자를 입력하시오: ");
-	scanf("%c", &c1);
+	//미션1 : 문자를 입력하시오: 에 입력하면 해당하는 아스카값 도출
+	char c;
+	printf("문자를 입력하시오 : ");
 
-	printf("%d \n", c1);
+	scanf("%c", &c);
 
-	float c2;
-	printf("면적을 제곱미터 단위로 입력하시오: ");
-	scanf("%f", &c2);
+	printf("아스카코드 : %d\n", c);
 
-	float result = c2 / 3.3058;
 
-	printf("%.2f제곱미터는 %.2f평입니다.", c2, result);
+	//미션2 : 면적을 제곱미터 단위로 입력하면 몇평인지 나오게 하는 것
+	float a;
+	printf("면적을 제곱미터 단위로 입력하시오 : ");
+	
+	scanf("%f", &a);
+
+	float result = a / 3.3058;
+
+	printf("%.2f제곱미터는 %.2f평입니다", a, result);
 
 	return 0;
 }
