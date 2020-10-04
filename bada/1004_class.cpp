77301e48188a422470c49ee59ca17613689a12ae
bada308 <71962076+bada308@users.noreply.github.com>
2020-10-04 20:38:31 +0900

    10/04 수업미션 (바다)

---
+++ bada/1004_class.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int solve() {
+
+	char c1;
+	printf("문자를 입력하시오: ");
+	scanf("%c", &c1);
+	printf("%c\n", c1);
+
+	return 0;
+}
+
+int solve() {
+	const float con1 = 3.3058;
+	float con2:
+	printf("면적을 제곱미터 단위로 입력하시오: ");
+	scanf("%d", &con2);
+	float con3 = con2 / con1;
+	printf("%d", con3)
+	
+}
